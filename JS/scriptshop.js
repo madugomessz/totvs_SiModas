ae4932e7f539d161144bb22af9554ac54e6893a4
pacheco-rfl <rafaelpacheco1324@yahoo.com>
2025-07-07 00:34:05 -0300

    algumas funcionalidades do shop

---
+++ JS/scriptshop.js
@@ -0,0 +1,97 @@
+const menu = document.getElementById("menu");
+const cartBtn = document.getElementById("cart-btn");
+const cartModal = document.getElementById("cart-modal");
+const cartItemsContainer = document.getElementById("cart-items");
+const cartTotal = document.getElementById("cart-total");
+const checkoutBtn = document.getElementById("checkout-btn");
+const closeModalBtn = document.getElementById("close-modal-btn");
+const cartCounter = document.getElementById("cart-count");
+const addressInput = document.getElementById("address");
+const addressWarn = document.getElementById("address-warn");
+
+
+let cart = [];
+
+//Abrir o modal do carrinho
+cartBtn.addEventListener("click", function () {
+    cartModal.style.display = "flex"
+    updateCartModal();
+})
+
+//Fechar o modal quando clicar fora 
+cartModal.addEventListener("click", function () {
+    if (event.target === cartModal) {
+        cartModal.style.display = "none"
+    }
+})
+
+//Fechar o modal quando clicar no botão fechar
+closeModalBtn.addEventListener("click", function () {
+    cartModal.style.display = "none"
+})
+
+//saber se clicou no botão do carrinho
+menu.addEventListener("click", function (event) {
+
+    let parentButton = event.target.closest(".add-to-cart-btn")
+
+    if (parentButton) {
+        const name = parentButton.getAttribute("data-name")
+        const price = parseFloat(parentButton.getAttribute("data-price"))
+
+        //Adicionar no carrinho.
+        addToCart(name, price)
+    }
+})
+
+//Funcão para adicionar no carrinho
+function addToCart(name, price) {
+    // alert(`o item é ${name}e o valor é ${price}`)
+    //verificando se o item ja está na lista
+    const resItem = cart.find(item => item.name === name)
+
+    if (resItem) {
+        //se o item ja existe, aumenta apenas a quantidade +1
+        resItem.quantity += 1;
+    } else {
+        cart.push({
+            name,
+            price,
+            quantity: 1,
+        })
+
+    }
+
+
+    updateCartModal()
+}
+
+//atualiza o carrinho
+function updateCartModal() {
+    cartItemsContainer.innerHTML = "";
+    let total = 0;
+
+
+    cart.forEach(item =>{ //percorre a lista e estrutura as divs no html, criando dentro do modal os nomes preços etc
+        const cartItemElement = document.createElement("div");
+        cartItemElement.classList.add("flex", "justify-between", "mb-4", "flex-col")
+
+        cartItemElement.innerHTML = `
+        <div class="flex items-center justify-between">
+        <div>
+            <div>
+            <p class="font-medium">${item.name}</p>
+            <p>Qtd: ${item.quantity}</p>
+            <p class="font-medium mt-2">R$ ${item.price}</p>
+            </div>
+
+
+            <button>
+                Remover
+            </button>
+        </div>
+    `
+
+    cartItemsContainer.appendChild(cartItemElement)
+    })
+}
